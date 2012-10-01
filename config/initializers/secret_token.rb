bb57cf2d2c7cc21d2fc5a4548cc4d49cb9b61530
masay0shi <masayoshi.cdm@gmail.com>
2012-09-30 17:32:37 -0700

    Inisial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+EmoSearch::Application.config.secret_token = '1e3cb68acc5ccbf1a31bd07dd306a06600bfd7ab22669f253a7d1170cd39261f35030582bdccc41211863a86e21b136de0fbe0abed30ea57cb36cffb732439c8'
